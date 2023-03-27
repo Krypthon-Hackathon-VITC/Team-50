b900aff2ce2289b0ce654c8bfd22a228c3ec70d8
chirag1020 <chiragcg2003@gmail.com>
2023-03-28 01:20:00 +0530

    doctor login integration success

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
-import { Questionare1 } from './Components/Questionare1';
-
+import { Questionare1 } from './components/Questionare1';
+import { useState } from 'react';
 import {MainLanding} from "./components/MainLanding";
 import {Choose} from './components/Choose'
 import { DoctorLogin }  from './components/DoctorLogin';
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+
+  const [ user, setLoginUser] = useState({})
+
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<MainLanding />} />
         <Route exact path="/choose" element={<Choose />} />
-        <Route exact path="/doctorLogin" element={<DoctorLogin />} />
+        <Route exact path="/DoctorLogin" element={<DoctorLogin setLoginUser={setLoginUser}/>} />
         <Route exact path="/questionare1" element={<Questionare1 />} />
       </Routes>
     </div>
