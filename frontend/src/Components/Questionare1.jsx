3f9c52c6d61fb78a8af62e3852c923bbe76124df
Ayush Priyadarshi <ayush.priyadarshi2021@vitstudent.ac.in>
2023-03-27 23:11:09 +0530

    commit

---
+++ frontend/src/Components/Questionare1.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Questionare.css';
+
+export const Questionare1 = () => {
+  return (
+     <>  
+      <nav className='navbar'>
+        <div className="logo">
+        <img id='artemis-logo' src={require('../Static/Images/logo.png')} alt="Artemis Logo" />
+        </div>
+        <div className="personLogo">
+        <img id='person-logo' src={require('../Static/Images/personLogo.png')} alt="Person Logo" />
+        </div>
+      </nav>
+      
+      <div className="box">
+       <div className="top">
+        <div className="top-left"><h1>Basics</h1> </div>
+        <div className="top-right"><h1>1/3</h1> </div>
+        </div>
+        <div className="main-box">
+          <div className="main-box-left">
+            <label htmlFor="FirstName">First Name</label>
+            <input type="text" />
+            <label htmlFor="Number">Phone Number</label>
+            <input type="number" />
+            <label htmlFor="Occupation">Occupation</label>
+            <input type="text" />
+          </div>
+          <div className="main-box-right">
+            <label htmlFor="LastName">Last Name</label>
+            <input type="text" />
+            <label htmlFor="DateofBirth">Date of Birth</label>
+            <input type="date" />
+            <label htmlFor="BloodGroup">Blood Group</label>
+            <input type="text" />
+          </div>
+        
+       </div>
+      </div>
+      </>
+  )
+}
+
+
