24103077d050f0855f19a3652c09c7b9587e26a1
Ayush Priyadarshi <ayush.priyadarshi2021@vitstudent.ac.in>
2023-03-28 05:11:41 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,8 @@ import { DoctorLogin }  from './components/DoctorLogin';
 import { Chat }  from './components/Chat';
 import { Route, Routes } from "react-router-dom";
 import { PatientLogin } from "./components/PatientLogin";
-import {Questionare2} from "./components/Questionare2"
+import {PatientRegister} from "./components/PatientRegister";
+
 
 function App() {
 
@@ -25,6 +26,7 @@ function App() {
         <Route exact path="/DoctorLogin" element={<DoctorLogin setLoginUser={setLoginUser}/>} />
         <Route exact path="/questionare1" element={<Questionare1 />} />
         <Route exact path="/PatientLogin" element={<PatientLogin setLoginUser={setLoginUser}/>} />
+        <Route exact path="/PatientRegister" element={<PatientRegister />} />
         <Route exact path="/questionare2" element={<Questionare2 />} />
         <Route exact path="/chat" element={<Chat username={email} />} />
       </Routes>
