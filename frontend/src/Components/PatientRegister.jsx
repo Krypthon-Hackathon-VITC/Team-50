9c5a3d1e247d25c3dda2c55baa4e9070d7b99899
RagedAbhi <abhilash.khadanga@gmail.com>
2023-03-28 08:44:29 +0530

    sync

--- frontend/src/Components/PatientRegister.jsx
+++ frontend/src/Components/PatientRegister.jsx
@@ -16,7 +16,7 @@ export const PatientRegister = () => {
         pass: '',
         name: ''
     })
-    
+
     const url = 'http://localhost:3001/api/patientsLogin/signup'
     const successMsg = 'succesfully registered. Login with your credentials!'
 
@@ -47,37 +47,37 @@ export const PatientRegister = () => {
 
 
     return (
-        <div> 
+        <div>
             <div className="artemis">
-            <img id='artemis-logo' src={require('../static/images/logo.png')} alt="Artemis Logo" />
+                <img id='artemis-logo' src={require('../static/images/logo.png')} alt="Artemis Logo" />
             </div>
-        <div className="container">
-            <div className="doc-img">
-            <img id="doc-img" src={require('../static/images/login-register.png')} alt="login-register" />
-            </div>
-            <div className="login-content">
-                <div className="login-head">
-                    <Link to="/PatientLogin" className="a-btn" style={currLink==="http://localhost:3000/PatientLogin"?{borderBottom: '3px solid #456188'}:null}>Login</Link>
-                    <Link to="/PatientRegister" className="a-btn" style={currLink==="http://localhost:3000/PatientRegister"?{borderBottom: '3px solid #456188'}:null}>Register</Link>
-                 
+            <div className="container">
+                <div className="doc-img">
+                    <img id="doc-img" src={require('../static/images/login-register.png')} alt="login-register" />
                 </div>
-                
-                <h1 id='welc'>Welcome to Artemis</h1>
-                <span id='ltya'>Register yourself</span>
-                <form className="login-credentials" onSubmit={submit}>
-                    <label className='EP' htmlFor="employee">Name</label>
-                    <input className='booxes' value={data.name} type="text" placeholder="name" id="name" name="name" onChange={(e) => handle(e)} />
-                    <label className='EP' htmlFor="employee">Email</label>
-                    <input className='booxes' value={data.email} type="text" placeholder="Email" id="email" name="email" onChange={(e) => handle(e)} />
-                    <label className='EP' htmlFor="password">Password</label>
-                    <input className='booxes' value={data.pass} type="password" placeholder="********" id="pass" name="pass" onChange={(e) => handle(e)} />
-                    <div className="lower-box">
+                <div className="login-content">
+                    <div className="login-head">
+                        <Link to="/PatientLogin" className="a-btn" style={currLink === "http://localhost:3000/PatientLogin" ? { borderBottom: '3px solid #456188' } : null}>Login</Link>
+                        <Link to="/PatientRegister" className="a-btn" style={currLink === "http://localhost:3000/PatientRegister" ? { borderBottom: '3px solid #456188' } : null}>Register</Link>
+
+                    </div>
+
+                    <h1 id='welc'>Welcome to Artemis</h1>
+                    <span id='ltya'>Register yourself</span>
+                    <form className="login-credentials" onSubmit={submit}>
+                        <label className='EP' htmlFor="employee">Name</label>
+                        <input className='booxes' value={data.name} type="text" placeholder="name" id="name" name="name" onChange={(e) => handle(e)} />
+                        <label className='EP' htmlFor="employee">Email</label>
+                        <input className='booxes' value={data.email} type="text" placeholder="Email" id="email" name="email" onChange={(e) => handle(e)} />
+                        <label className='EP' htmlFor="password">Password</label>
+                        <input className='booxes' value={data.pass} type="password" placeholder="********" id="pass" name="pass" onChange={(e) => handle(e)} />
+                        <div className="lower-box">
                             <button id="DoclogIn" type="submit">Sign Up</button>
                             <span id='feed'></span>
                         </div>
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
         </div>
     );
 }
