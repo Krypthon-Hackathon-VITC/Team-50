7661f26f7798bdff92deac40de5f29400e2d6f90
chirag1020 <chiragcg2003@gmail.com>
2023-04-03 22:02:14 +0530

    ahhh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,41 @@
-import './App.css';
-import { Questionare1 } from './components/Questionare1';
-import { Questionare2 } from './components/Questionare2';
-import { Questionare3 } from './components/Questionare3';
-import { useState } from 'react';
+import "./App.css";
+import { Questionare1 } from "./components/Questionare1";
+import { Questionare2 } from "./components/Questionare2";
+import { Questionare3 } from "./components/Questionare3";
+import { useState } from "react";
 import { MainLanding } from "./components/MainLanding";
-import { Choose } from './components/Choose'
-import { DoctorLogin } from './components/DoctorLogin';
-import { Chat } from './components/Chat';
+import { Choose } from "./components/Choose";
+import { DoctorLogin } from "./components/DoctorLogin";
+import { Chat } from "./components/Chat";
 import { Route, Routes } from "react-router-dom";
 import { PatientLogin } from "./components/PatientLogin";
-import {PatientRegister} from "./components/PatientRegister";
-import {DoctorLanding} from "./components/DoctorLanding";
-import {PatientLanding} from "./components/PatientLanding";
-import { DocLanding } from './components/DocLanding';
-import { PatientInfo } from './components/PatientInfo';
-
-
+import { PatientRegister } from "./components/PatientRegister";
+import { DoctorLanding } from "./components/DoctorLanding";
+import { PatientLanding } from "./components/PatientLanding";
+import { DocLanding } from "./components/DocLanding";
+import { PatientInfo } from "./components/PatientInfo";
 
 function App() {
+  const [LoginUser, setLoginUser] = useState({});
 
-  const [LoginUser, setLoginUser] = useState({})
-
-  const email = "abhi@gmail.com"
+  const email = "abhi@gmail.com";
 
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<MainLanding />} />
         <Route exact path="/choose" element={<Choose />} />
-        <Route exact path="/DoctorLogin" element={<DoctorLogin setLoginUser={setLoginUser} />} />
+        <Route
+          exact
+          path="/DoctorLogin"
+          element={<DoctorLogin setLoginUser={setLoginUser} />}
+        />
         <Route exact path="/questionare1" element={<Questionare1 />} />
-        <Route exact path="/PatientLogin" element={<PatientLogin setLoginUser={setLoginUser} />} />
+        <Route
+          exact
+          path="/PatientLogin"
+          element={<PatientLogin setLoginUser={setLoginUser} />}
+        />
         <Route exact path="/PatientRegister" element={<PatientRegister />} />
         <Route exact path="/questionare2" element={<Questionare2 />} />
         <Route exact path="/chat" element={<Chat username={email} />} />
